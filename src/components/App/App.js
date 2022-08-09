15b171d98d9a58269524fd9419d0974752a345cd
Jordan Kaiser <jordan.ross.kaiser@gmail.com>
2022-08-09 06:26:31 -0500

    Initial commit.

---
+++ src/components/App/App.js
@@ -0,0 +1,68 @@
+import logo from '../../img/logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <li>
+                <Link to="/history">History</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Routes> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Routes>
+            <Route path="/search"  element={<About/>} />
+            <Route path="/history"  element={<Users/>} />
+            <Route path="/"  element={<Home/>} />
+          </Routes>
+        </div>
+      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export default App;
