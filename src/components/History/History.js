acf4abf0a4838045b5b771f6fe1d1d4c0087ce0a
Jordan Kaiser <jordan.ross.kaiser@gmail.com>
2022-08-09 14:38:06 -0500

    History page now displaying user searches.

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,5 +1,28 @@
+import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
+
+
 export default function History() {
-  return (
-    <div>history</div>
-  )
+  // Get searches from state.
+  const searchState = useSelector(state => state.searches);
+
+  const SearchItemsMarkup = () => {
+    // Generate list of user's searched terms.
+    return searchState.searches.map((search, index) => <li key={`${index}-${search}`}>{search}</li>);
+  }
+
+  if (searchState.searches.length > 0) {
+    return (
+      <>
+        <h1>Search History</h1>
+        <ul>
+          <SearchItemsMarkup />
+        </ul>
+      </>
+    )
+  } else {
+    return (
+      <div>No search history found. <Link to='/search'>Preform a search</Link>.</div>
+    )
+  }
 }
